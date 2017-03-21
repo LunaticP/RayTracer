52fe378ac12931683c1cf0ff7c0430945fa8227c
Philippe GOURRAND <pgourran@e1r1p3.42.fr>
2017-03-21 17:10:07 +0100

    yoloy segfault obj

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jplevy <jplevy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/08 01:37:39 by jplevy            #+#    #+#             */
-/*   Updated: 2017/03/20 08:32:08 by pgourran         ###   ########.fr       */
+/*   Updated: 2017/03/21 17:09:43 by pgourran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,44 @@ cl_float4		vec_norm(cl_float4 vec)
 	return (norm);
 }
 
-t_scene	ft_init_scene(void)
+void	insert_obj(t_obj *tab, t_list *obj)
+{
+	int		i;
+	t_triangle	*t;
+	t_list		*k;
+
+	k = obj;
+	i = 0;
+	while(k->next)
+	{
+		ft_putstr("yolo");
+		tab[++i].type = triangle;
+		t = k->content;
+		tab[i].col = 0xFFFFFF;
+		tab[i].p1.x = t->p1.x;
+		printf("%f", tab[i].p1.x);
+		tab[i].p1.y = t->p1.y;
+		tab[i].p1.z = t->p1.z;
+		tab[i].p2.x = t->p2.x;
+		tab[i].p2.y = t->p2.y;
+		tab[i].p2.z = t->p2.z;
+		tab[i].p3.x = t->p3.x;
+		tab[i].p3.y = t->p3.y;
+		tab[i].p3.z = t->p3.z;
+		tab[i].dir.x = (t->vn1.x + t->vn2.x + t->vn3.x) / 3;
+		tab[i].dir.y = (t->vn1.y + t->vn2.y + t->vn3.y) / 3;
+		tab[i].dir.z = (t->vn1.z + t->vn2.z + t->vn3.z) / 3;
+		k = k->next;
+	}
+}
+
+t_scene	ft_init_scene(t_list *obj)
 {
 	t_scene	ret;
-
+	int		lenobj;
 	ret.cam.ori.x = 0;
 	ret.cam.ori.y = 0;
-	ret.cam.ori.z = 0;
+	ret.cam.ori.z = -20;
 
 	ret.cam.dirx.x = 1.0;
 	ret.cam.dirx.y = 0.0;
@@ -60,7 +91,7 @@ t_scene	ft_init_scene(void)
 	ret.cam.p.z = 5;
 
 	ret.light = ft_memalloc(sizeof(t_obj) * 6);
-	ret.light[0].pos.x = 0;
+	ret.light[0].pos.x = 10;
 	ret.light[0].pos.y = 2.5;
 	ret.light[0].pos.z = -1.0;
 
@@ -68,24 +99,24 @@ t_scene	ft_init_scene(void)
 	ret.light[0].type = light;
 	ret.light[0].r = 100;
 
-	ret.light[1].pos.x = -2.5;
-	ret.light[1].pos.y = -2.5;
-	ret.light[1].pos.z = -1.0;
+	ret.light[1].pos.x = -12.5;
+	ret.light[1].pos.y = -12.5;
+	ret.light[1].pos.z = -11.0;
 
 	ret.light[1].col = 0xFF00;
 	ret.light[1].type = light;
 	ret.light[1].r = 100;
 
-	ret.light[2].pos.x = 2.5;
-	ret.light[2].pos.y = -2.5;
-	ret.light[2].pos.z = -1.0;
+	ret.light[2].pos.x = 12.5;
+	ret.light[2].pos.y = -12.5;
+	ret.light[2].pos.z = -11.0;
 
 	ret.light[2].col = 0xFF0000;
 	ret.light[2].type = light;
 	ret.light[2].r = 100;
 
 	ret.light[3].pos.x = -20.0;
-	ret.light[3].pos.y = 0.0;
+	ret.light[3].pos.y = 10.0;
 	ret.light[3].pos.z = 25.0;
 
 	ret.light[3].col = 0xFFFFFF;
@@ -93,7 +124,7 @@ t_scene	ft_init_scene(void)
 	ret.light[3].r = 100;
 
 	ret.light[4].pos.x = 20.0;
-	ret.light[4].pos.y = 0.0;
+	ret.light[4].pos.y = 10.0;
 	ret.light[4].pos.z = 25.0;
 
 	ret.light[4].col = 0xFFFFFF;
@@ -101,8 +132,8 @@ t_scene	ft_init_scene(void)
 	ret.light[4].r = 100;
 
 	ret.light[5].type = end;
-
-	ret.obj = ft_memalloc(sizeof(t_obj) * 6);
+	lenobj = ft_lstlen(obj);
+	ret.obj = ft_memalloc(sizeof(t_obj) * (6 + lenobj));
 
 	ret.obj[0].pos.x = 4.0;
 	ret.obj[0].pos.y = 8.0;
@@ -138,21 +169,13 @@ t_scene	ft_init_scene(void)
 	ret.obj[4].type = plan;
 
 
-	ret.obj[1].p1.x = -6.0;
-	ret.obj[1].p1.y = -3.0;
-	ret.obj[1].p1.z = 7.0;
-	ret.obj[1].p2.x = 2.0;
-	ret.obj[1].p2.y = -3.0;
-	ret.obj[1].p2.z = 7.0;
-	ret.obj[1].p3.x = -2.0;
-	ret.obj[1].p3.y = 2.0;
-	ret.obj[1].p3.z = 10.0;
+	ret.obj[1].pos.x = 10.0;
+	ret.obj[1].pos.y = 10.0;
+	ret.obj[1].pos.z = 10.0;
 
-	ret.obj[1].dir.x = 0.0;
-	ret.obj[1].dir.y = 2.5;
-	ret.obj[1].dir.z = -1.0;
+	ret.obj[1].r = 1.5;
 	ret.obj[1].col = 0xFFFFFF;
-	ret.obj[1].type = triangle;
+	ret.obj[1].type = sphere;
 
 	ret.obj[2].pos.x = 2.0;
 	ret.obj[2].pos.y = 3.0;
@@ -162,8 +185,8 @@ t_scene	ft_init_scene(void)
 	ret.obj[2].alpha = M_PI / 16.0;
 	ret.obj[2].col = 0xFFFFFF;
 	ret.obj[2].type = sphere;
-
-	ret.obj[5].type = end;
+//	insert_obj(&ret.obj[5], obj);
+	ret.obj[5 + lenobj].type = end;
 	return (ret);
 }
 /*
@@ -195,7 +218,7 @@ int		main(int ac, char **av)
 	if (!(ocl_new_prog("./cl_src/rt.cl", 0x1000000 , &(mlx.prog))))
 		return (0);
 	obj = get_obj("ico.obj");
-	mlx.s = ft_init_scene();
+	mlx.s = ft_init_scene(obj);
 	mlx.mlx = mlx_init();
 	mlx.win = mlx_new_window(mlx.mlx, WIDTH, HEIGHT, "rtvocl");
 	mlx.img = mlx_new_image(mlx.mlx, WIDTH, HEIGHT);
@@ -204,7 +227,8 @@ int		main(int ac, char **av)
 	pws[0] = WIDTH;
 	pws[1] = HEIGHT;
 	ocl_new_kernel(&(mlx.prog), 4, pws, "norowowowd", "raytracer", WIDTH * HEIGHT
-		* sizeof(int), mlx.p, sizeof(t_cam), &(mlx.s.cam), sizeof(t_obj) * 6, 
+		* sizeof(int), mlx.p, sizeof(t_cam), 
+		&(mlx.s.cam), sizeof(t_obj) * (6 + ft_lstlen(obj)), 
 		mlx.s.obj, sizeof(t_obj) * 6, mlx.s.light, 2);
 	ocl_enqueue_kernel(&(mlx.prog), "raytracer");
 	mlx_put_image_to_window(mlx.mlx, mlx.win, mlx.img, 0, 0);
--- src/parseobj.c
+++ src/parseobj.c
@@ -6,7 +6,7 @@
 /*   By: pgourran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/20 08:15:32 by pgourran          #+#    #+#             */
-/*   Updated: 2017/03/20 10:58:07 by pgourran         ###   ########.fr       */
+/*   Updated: 2017/03/21 17:07:56 by pgourran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,26 +42,41 @@ cl_float	ft_get_value(char *str)
 	return(res);
 }
 
+cl_float4	get_content(t_list *list, int num)
+{
+	num++;
+	t_list *ptr;
+	cl_float4	*fl;
 
+	ptr = list;
+	while(--num && ptr->next)
+		ptr = ptr->next;
+	fl = ptr->content;
+	return(*fl);
+}
 
 t_list		*get_obj(char *path)
 {
 	t_list		*file;
 	char		**data;
 	t_list		*obj;
-//	t_triangle	tri;
+	t_triangle	tri;
+	t_triangle	*tro;
 	int			y;
 	int			x;
 	file = get_file(path);
 	data = ft_lsttotab(file);
 	t_list		*pts;
+	t_list		*pns;
+	t_list		*tris;
 	cl_float4	p;
-	cl_float4	*l;
+	cl_float4	pn;
 //	ft_freelst(&file);
 	y = 0;
-	ft_puttab(data);
 	obj = NULL;
 	pts = NULL;
+	pns = NULL;
+	tris = NULL;
 	while(data[y][0] != 'v' || data[y][1] != ' ')
 		y++;
 	data += y;
@@ -84,11 +99,65 @@ t_list		*get_obj(char *path)
 		ft_lstpushback(&pts, &p, sizeof(p));
 		y++;
 	}
-	while(pts->next)
+	while(data[y][0] == 'v' && data[y][1] == 'n')
 	{
-		l = pts->content;
-		printf("%f : %f : %f\n",l->x,l->y,l->z);
-		pts = pts->next;
+		x = 3;
+		pn.x = ft_get_value(&data[y][x]);
+		while(data[y][x] != ' ')
+			x++;
+		x++;
+		pn.y = ft_get_value(&data[y][x]);
+		while(data[y][x] != ' ')
+			x++;
+		x++;
+		pn.z = ft_get_value(&data[y][x]);
+		ft_lstpushback(&pns, &pn, sizeof(pn));
+		y++;
+	}
+	y++;
+	while(data[y] && data[y][0] == 'f' && data[y][1] == ' ')
+	{
+		x = 2;
+		tri.p1 = get_content(pts, ft_atoi(&data[y][x]));
+		x += 2;
+		while(data[y][x] != '/')
+			x++;
+		x++;
+		tri.vn1 = get_content(pns, ft_atoi(&data[y][x]));
+		while(data[y][x] != ' ')
+			x++;
+		x++;
+		tri.p2 = get_content(pts, ft_atoi(&data[y][x]));
+		x += 2;
+		while(data[y][x] != '/')
+			x++;
+		x++;
+		tri.vn2 = get_content(pns, ft_atoi(&data[y][x]));
+		while(data[y][x] != ' ')
+			x++;
+		x++;
+		tri.p3 = get_content(pts, ft_atoi(&data[y][x]));
+		x += 2;
+		while(data[y][x] != '/')
+			x++;
+		x++;
+		tri.vn3 = get_content(pns, ft_atoi(&data[y][x]));
+		while(data[y][x] != ' ')
+			x++;
+		x++;
+		ft_lstpushback(&tris, &tri, sizeof(tri));
+		y++;
+	}
+	while(tris->next)
+	{
+		tro = tris->content;
+//		printf("%f : %f : %f\n",tro->p1.x,tro->p1.y,tro->p1.z);
+//		printf("%f : %f : %f\n",tro->p2.x,tro->p2.y,tro->p2.z);
+//		printf("%f : %f : %f\n",tro->p3.x,tro->p3.y,tro->p3.z);
+//		printf("%f : %f : %f\n",tro->vn1.x,tro->vn1.y,tro->vn1.z);
+//		printf("%f : %f : %f\n",tro->vn2.x,tro->vn2.y,tro->vn2.z);
+//		printf("%f : %f : %f\n",tro->vn3.x,tro->vn3.y,tro->vn3.z);
+		tris = tris->next;
 		
 	}
 	ft_putstr("\033[32;0m ");
