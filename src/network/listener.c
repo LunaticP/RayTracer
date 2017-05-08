1dbe3f7ed6e75b9771dfa71ff265d1d3c68ba915
MajorTom327 <vthomas@e1r1p12.42.fr>
2017-05-08 18:22:52 +0200

    end of communication

--- src/network/listener.c
+++ src/network/listener.c
@@ -6,14 +6,13 @@
 /*   By: vthomas <vthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/05 17:46:07 by vthomas           #+#    #+#             */
-/*   Updated: 2017/05/08 17:52:41 by vthomas          ###   ########.fr       */
+/*   Updated: 2017/05/08 18:22:28 by vthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <rt_network.h>
 #include <libft.h>
 #include <pthread.h>
-#include <errno.h>
 
 static void print_mem(char *buf, int l)
 {
@@ -31,16 +30,11 @@ static void print_mem(char *buf, int l)
 
 int			parse_msg(char *buf, int len, t_client *c)
 {
-	int	type;
 	int	current;
 	int	tmp;
 
-	print_info("Parsing start");
 	ft_memcpy((void *)&(c->type), (void *)c->buf, sizeof(int));
-	//type = (int)*(c->buf);
-	print_warning("type set");
 	len = read(c->sock, buf, BUFF_LEN);
-	print_log("Readed");
 	len = (int)*buf;
 	print_info(ft_itoa(len));
 	current = 0;
@@ -54,11 +48,8 @@ int			parse_msg(char *buf, int len, t_client *c)
 		c->buf = (char *)memjoin(c->buf, buf, current - tmp, BUFF_LEN);
 		ft_bzero(buf, BUFF_LEN);
 	}
-	print_info("readed !");
 	c->len = len;
-	//c->type = type;
-	print_mem(c->buf, c->len);
-	return (type);
+	return (c->type);
 }
 
 /*
@@ -67,25 +58,15 @@ int			parse_msg(char *buf, int len, t_client *c)
 void client_listener(t_client *c)
 {
 	int		ret;
-	//char	buf[BUFF_LEN];
 
 	print_log("Thread started");
 	send_message(msg_part, "Hello World", 12, 0);
 	c->buf = ft_strnew(4);
 	while ((ret = read(c->sock, c->buf, BUFF_LEN)))
 	{
-		print_info("Going parse");
-		//printf("%02X%02X%02X%02X\n"c->buf[0])
-		print_mem(c->buf, 4);
 		ret = parse_msg(c->buf, ret, c);
-//		print_mem(c->buf, c->len);
+		print_mem(c->buf, c->len);
 	}
-//	while ((ret = read(c->sock, (c->buf), BUFF_LEN)) > 0)
-//	{
-//		parse_msg(c->buf, ret, &(c->msg));
-//		print_mem(c->msg.buf, c->msg.len);
-//		//print_info(c->buf);
-//	}
 	print_info("Disconnected");
 	close(c->sock);
 }
