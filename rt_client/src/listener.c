ead9f617bf16517cdc6a15c8606d3b79cb1346c5
MajorTom327 <vthomas@e1r2p1.42.fr>
2017-05-09 15:57:31 +0200

    segfault client

--- rt_client/src/listener.c
+++ rt_client/src/listener.c
@@ -6,7 +6,7 @@
 /*   By: vthomas <vthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/05 18:59:56 by vthomas           #+#    #+#             */
-/*   Updated: 2017/05/09 12:47:00 by aviau            ###   ########.fr       */
+/*   Updated: 2017/05/09 15:49:18 by vthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,22 +30,24 @@ void print_mem(unsigned char *buf, int l)
 
 int			parse_msg(unsigned char *buf, int len, t_client *c)
 {
-	int	type;
-	int	current;
-	int	tmp;
+	int				type;
+	int				current;
+	int				tmp;
+	unsigned char	*buf_tmp;
 
 	type = *(int *)(&buf[0]);
 	len = recv(c->sock, buf, BUFF_LEN, 0);
 	len = *(int *)(&buf[0]);
 	current = 0;
-	ft_bzero(buf, BUFF_LEN);
+	buf_tmp = (unsigned char *)ft_strnew(len);
+	//ft_bzero(buf, BUFF_LEN);
 	c->buf = (unsigned char *)ft_strnew(0);
 	while (current < len)
 	{
-		tmp = recv(c->sock, buf, BUFF_LEN, 0);
-		c->buf = (unsigned char *)memjoin(c->buf, buf, current, BUFF_LEN);
+		tmp = read(c->sock, buf_tmp, len);
+		c->buf = (unsigned char *)memjoin(c->buf, buf_tmp, current, len);
 		current += tmp;
-		ft_bzero(buf, BUFF_LEN);
+		ft_bzero(buf_tmp, len);
 	}
 	c->len = len;
 	c->type = type;
@@ -62,12 +64,14 @@ void	rt_listener(t_client *c)
 {
 	unsigned char	buf[BUFF_LEN + 1];
 	int		l;
+
 	buf[BUFF_LEN] = 0;
 	while ((l = (recv(c->sock, buf, BUFF_LEN, 0))))
 	{
 		l = parse_msg(buf, l, c);
 //		print_mem(c->buf, c->len);
-		print_log("turn");
+		print_log("turn and go to render lol");
+		print_info(ft_itoa(l));
 		parse_callback(c, l);
 		//send_message(msg_tex, "OK", 3, c);
 	}
