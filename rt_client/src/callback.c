b0882780b67f3a62f18edf75913ce17a1e1c2768
MajorTom327 <vthomas@e1r1p3.42.fr>
2017-05-11 15:34:26 +0200

    fix number object saved error

--- rt_client/src/callback.c
+++ rt_client/src/callback.c
@@ -6,7 +6,7 @@
 /*   By: vthomas <vthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/08 21:19:17 by vthomas           #+#    #+#             */
-/*   Updated: 2017/05/11 14:39:19 by aviau            ###   ########.fr       */
+/*   Updated: 2017/05/11 15:31:05 by vthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,22 +49,22 @@ void	render(t_data *d, t_ocl_prog *prog, int line, t_client *c)
 //	img_file(d->img, "rendu.ppm", d);
 }
 
-t_data	str_to_data(unsigned char *str)
+t_data	*str_to_data(unsigned char *str)
 {
-	t_data	data;
+	t_data	*data;
 
-	data = *(t_data *)ft_memalloc(sizeof(t_data));
-	data.size = *(int *)(&str[0]);
-	data.n_t = *(int *)(&str[4]);
-	data.n_o = *(int *)(&str[8]);
-	data.n_l = *(int *)(&str[12]);
-	data.tex = (int *)ft_memalloc(sizeof(int) * data.n_t);
-	ft_memcpy(data.tex, &str[16], sizeof(int) * data.n_t);
-	ft_memcpy(&data.cam, &str[16 + sizeof(int) * data.n_t], sizeof(t_cam));
-	data.obj = (t_obj *)ft_memalloc(sizeof(t_obj) * data.n_o);
-	ft_memcpy(data.obj, &str[16 + sizeof(int) * data.n_t + sizeof(t_cam)] , sizeof(t_obj) * data.n_o);
-	data.light = (t_obj *)ft_memalloc(sizeof(t_obj) * data.n_l);
-	ft_memcpy(data.light, &str[16 + sizeof(int) * data.n_t + sizeof(t_cam) + sizeof(t_obj) * data.n_o], sizeof(t_obj) * data.n_l);
+	data = (t_data *)ft_memalloc(sizeof(t_data));
+	data->size = *(int *)(&str[0]);
+	data->n_t = *(int *)(&str[4]);
+	data->n_o = *(int *)(&str[8]);
+	data->n_l = *(int *)(&str[12]);
+	data->tex = (int *)ft_memalloc(sizeof(int) * data->n_t);
+	ft_memcpy(data->tex, &str[16], sizeof(int) * data->n_t);
+	ft_memcpy(&data->cam, &str[16 + sizeof(int) * data->n_t], sizeof(t_cam));
+	data->obj = (t_obj *)ft_memalloc(sizeof(t_obj) * data->n_o);
+	ft_memcpy(data->obj, &str[16 + sizeof(int) * data->n_t + sizeof(t_cam)] , sizeof(t_obj) * data->n_o);
+	data->light = (t_obj *)ft_memalloc(sizeof(t_obj) * data->n_l);
+	ft_memcpy(data->light, &str[16 + sizeof(int) * data->n_t + sizeof(t_cam) + sizeof(t_obj) * data->n_o], sizeof(t_obj) * data->n_l);
 	return (data);
 }
 
@@ -96,36 +96,36 @@ void	callback_render(t_client *c)
 	print_info(ft_itoa(d->n_o));
 	print_info("go for render");
 	render(d, p, *(int *)c->buf, c);
-	print_info("go for image"); 
+	print_info("go for image");
 //	ocl_finish(prog);
 }
 
 void	callback_init(t_client *c)
 {
 	t_ocl_prog	prog;
-	t_data		d;
+	t_data		*d;
 	size_t		pws[2];
 
 	if (!(ocl_new_prog("./cl_src/rt.cl", 0x1000000 , &prog)))
 		exit(-1);
 	d = str_to_data(c->buf);
-	d.width = 1280;
-	d.height = 720;
-	d.scale = 1;
-	d.line = 1;
-	d.cam.chunk.y = 0;//d.line - 1;
-	d.cam.chunk.x = 0;
-	d.cam.viewplane.z = d.scale;
-	d.img = (unsigned char *)ft_memalloc(d.width * d.height * sizeof(int));
-	pws[0] = d.width/* / d.scale*/;
-	pws[1] = d.height;
+	d->width = 1280;
+	d->height = 720;
+	d->scale = 1;
+	d->line = 1;
+	d->cam.chunk.y = 0;//d.line - 1;
+	d->cam.chunk.x = 0;
+	d->cam.viewplane.z = d->scale;
+	d->img = (unsigned char *)ft_memalloc(d->width * d->height * sizeof(int));
+	pws[0] = d->width/* / d.scale*/;
+	pws[1] = d->height;
 	ocl_new_kernel(&prog, 5, pws, "norowowowowd", "raytracer", \
-			sizeof(int) * d.width * d.height, d.img, \
-			sizeof(t_cam), &(d.cam), \
-			sizeof(t_obj) * d.n_o, d.obj, \
-			sizeof(t_obj) * d.n_l, d.light, \
-			sizeof(int) * (d.tex[0] + 1), d.tex, 2);
-	save_data(0, &d);
+			sizeof(int) * d->width * d->height, d->img, \
+			sizeof(t_cam), &(d->cam), \
+			sizeof(t_obj) * d->n_o, d->obj, \
+			sizeof(t_obj) * d->n_l, d->light, \
+			sizeof(int) * (d->tex[0] + 1), d->tex, 2);
+	save_data(0, d);
 	save_oprg(0, &prog);
-	print_info("callback initiated"); 
+	print_info("callback initiated");
 }
